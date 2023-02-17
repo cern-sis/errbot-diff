70a1a49a09bfd637271c5a348f09e891504be35b
jimil749 <jimildesai42@gmail.com>
2023-02-17 16:58:18 +0100

    first test to see git diff
    Signed-off-by: jimil749 <jimildesai42@gmail.com>

---
+++ errbot-diff.py
@@ -0,0 +1,87 @@
+from errbot import BotPlugin, arg_botcmd, botcmd, webhook
+
+
+class Errbot-diff(BotPlugin):
+    """
+    Errbot plugin to display diff between two namespaces
+    """
+
+    def activate(self):
+        """
+        Triggers on plugin activation
+
+        You should delete it if you're not using it to override any default behaviour
+        """
+        super(Errbot-diff, self).activate()
+
+    def deactivate(self):
+        """
+        Triggers on plugin deactivation
+
+        You should delete it if you're not using it to override any default behaviour
+        """
+        super(Errbot-diff, self).deactivate()
+
+    def get_configuration_template(self):
+        """
+        Defines the configuration structure this plugin supports
+
+        You should delete it if your plugin doesn't use any configuration like this
+        """
+        return {
+            "EXAMPLE_KEY_1": "Example value",
+            "EXAMPLE_KEY_2": ["Example", "Value"],
+        }
+
+    def check_configuration(self, configuration):
+        """
+        Triggers when the configuration is checked, shortly before activation
+
+        Raise a errbot.ValidationException in case of an error
+
+        You should delete it if you're not using it to override any default behaviour
+        """
+        super(Errbot-diff, self).check_configuration(configuration)
+
+    def callback_connect(self):
+        """
+        Triggers when bot is connected
+
+        You should delete it if you're not using it to override any default behaviour
+        """
+        pass
+
+    def callback_message(self, message):
+        """
+        Triggered for every received message that isn't coming from the bot itself
+
+        You should delete it if you're not using it to override any default behaviour
+        """
+        pass
+
+    def callback_botmessage(self, message):
+        """
+        Triggered for every message that comes from the bot itself
+
+        You should delete it if you're not using it to override any default behaviour
+        """
+        pass
+
+    # Passing split_args_with=None will cause arguments to be split on any kind
+    # of whitespace, just like Python's split() does
+    @botcmd
+    def diff(self, message, args):
+        # first - clone the repo
+        repo_url = "https://github.com/cern-sis/kubernetes.git"
+        remote_branch_1 = "origin/master_output"
+        remote_branch_2 = "origin/"+args
+        directory=args
+        local_path = "kubernetes"
+
+        subprocess.run(["git", "clone", repo_url, local_path])
+        subprocess.run(["git", "fetch", "origin"], cwd=local_path)
+        cmd = ["git", "diff", f"{remote_branch_1}..{remote_branch_2}", "--", directory]
+        result = subprocess.run(cmd, cwd=local_path, capture_output=True, text=True)
+
+        return result.stdout
+
