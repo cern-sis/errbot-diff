ed789ef4aac3afe064e6cbbd5b1437a495e824c3
jimil749 <jimildesai42@gmail.com>
2023-02-20 14:49:10 +0100

    add pat for cloning
    Signed-off-by: jimil749 <jimildesai42@gmail.com>

--- errbotdiff.py
+++ errbotdiff.py
@@ -1,77 +1,12 @@
-from errbot import BotPlugin, arg_botcmd, botcmd
+from errbot import BotPlugin, botcmd
 import subprocess
-
+import os
 
 class Errbotdiff(BotPlugin):
-    """
-    git diff
-    """
-
-    def activate(self):
-        """
-        Triggers on plugin activation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Errbotdiff, self).activate()
-
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Errbotdiff, self).deactivate()
-
-    def get_configuration_template(self):
-        """
-        Defines the configuration structure this plugin supports
-
-        You should delete it if your plugin doesn't use any configuration like this
-        """
-        return {
-            "EXAMPLE_KEY_1": "Example value",
-            "EXAMPLE_KEY_2": ["Example", "Value"],
-        }
-
-    def check_configuration(self, configuration):
-        """
-        Triggers when the configuration is checked, shortly before activation
-
-        Raise a errbot.ValidationException in case of an error
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Errbotdiff, self).check_configuration(configuration)
-
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
     @botcmd
     def diff(self, message, args):
-        # first - clone the repo
-        repo_url = "https://github.com/cern-sis/kubernetes.git"
+        gh_pat = os.environ["GITHUB_PAT"]
+        repo_url = "https://"+gh_pat+"@github.com/cern-sis/kubernetes.git"
         remote_branch_1 = "origin/master_output"
         remote_branch_2 = "origin/"+args
         directory=args
