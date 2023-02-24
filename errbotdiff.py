0d006535d40ff58ea6009944d39f82004350e598
jimil749 <jimildesai42@gmail.com>
2023-02-24 14:13:14 +0100

    add yeild for more info
    Signed-off-by: jimil749 <jimildesai42@gmail.com>

--- errbotdiff.py
+++ errbotdiff.py
@@ -6,20 +6,22 @@ import os
 class Errbotdiff(BotPlugin):
     @botcmd(template="diff")
     def diff(self, message, args):
+        yeild "Cloning repo kubernetes ..."
         gh_pat = os.environ["GITHUB_PAT"]
         repo_url = f"https://cern-sis-bot:{gh_pat}@github.com/cern-sis/kubernetes.git"
         remote_branch_1 = "origin/master_output"
         remote_branch_2 = "origin/"+args
         directory=args
         local_path = "kubernetes"
-
+        
         subprocess.run(["git", "clone", repo_url, local_path])
         subprocess.run(["git", "fetch", "origin"], cwd=local_path)
         cmd = ["git", "diff", "--name-only", f"{remote_branch_1}..{remote_branch_2}", "--", directory]
         result = subprocess.run(cmd, cwd=local_path, capture_output=True, text=True)
         
         changed_files = result.stdout.splitlines()
-
+        
+        yeild "Generating the diff..."
         for file_path in changed_files:
             cmd = ["git", "diff", f"{remote_branch_1}..{remote_branch_2}", "--", file_path]
             result = subprocess.run(cmd, cwd=local_path, capture_output=True, text=True)
